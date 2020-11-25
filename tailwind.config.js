d241f5e923e4bfc63933a4ebd0beddbaa0799324
Kim Jongkwan <kimjongkwan@Kims-MacBook-Pro.local>
2020-11-25 22:16:31 +0900

    기초틀잡기

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,21 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      zIndex: {
+        '1': 1,
+        '2': 2,
+        '3': 3,
+        '4': 4,
+        '5': 5,
+        '6': 6
+      }
+    }
   },
   variants: {
     extend: {},
   },
   plugins: [],
+  experimental: {
+  }
 }
