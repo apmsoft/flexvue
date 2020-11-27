cc9d7d16a782d4656175f1b0a87dcb4f184e2074
Kim Jongkwan <kimjongkwan@Kims-MacBook-Pro.local>
2020-11-27 19:00:51 +0900

    css 1. overflow-unset 스크린사이즈별로 추가
    js
    1. minHeight 값 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,14 +9,21 @@ module.exports = {
         '3': 3,
         '4': 4,
         '5': 5,
-        '6': 6
+      },
+      minHeight : {
+        '1/2' : '50%',
+        '1/3' : '33.333333%',
+        '2/3' : '66.666667%',
+        '1/2vh' : '50vh',
+        '1/3vh' : '33vh',
+        '2/3vh' : '66vh'
       }
-    }
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+
+    },
   },
   plugins: [],
-  experimental: {
-  }
 }
