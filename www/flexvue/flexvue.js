cdd29e35935d2cb473635b200306d87a26a9bf78
Kim Jongkwan <kimjongkwan@Kims-MacBook-Pro.local>
2021-03-14 22:07:16 +0900

    d

--- www/flexvue/flexvue.js
+++ www/flexvue/flexvue.js
@@ -1,6 +1,4 @@
-
 const config = {
-    baseUrl    : `../..`,
     version    : '0.1.1',
     int_version: 1,
     debug      : ['d','i','v','w','e'], // 출력하고자 하는 디버그 모드 선택
@@ -173,7 +171,6 @@ class Log {
 class ProgressBar {
     static progressObj = null;
     constructor(){
-        // var loading = '<div id="p2" class="mdl-progress mdl-js-progress mdl-progress__indeterminate is-upgraded" style="width:100vw;" data-upgraded=",MaterialProgress"><div class="progressbar bar bar1" style="width: 0%;"></div><div class="bufferbar bar bar2" style="width: 100%;"></div><div class="auxbar bar bar3" style="width: 0%;"></div></div>';
         const loading ='<svg width="40px"  height="40px"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="lds-rolling" style="background: none;"><circle cx="50" cy="50" fill="none" ng-attr-stroke="{{config.color}}" ng-attr-stroke-width="{{config.width}}" ng-attr-r="{{config.radius}}" ng-attr-stroke-dasharray="{{config.dasharray}}" stroke="#55ad15" stroke-width="10" r="35" stroke-dasharray="164.93361431346415 56.97787143782138" transform="rotate(347.727 50.0001 50.0001)"><animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 50;360 50 50" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animateTransform></circle></svg>';
 
         Promise.resolve((document.querySelector('body').insertAdjacentHTML('afterend',`<div id="loading" class="loading">${loading}</div>`)))
