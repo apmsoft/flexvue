edb4db50f2690270af3d40698bc56f328fa87c9e
Kim Jongkwan <kimjongkwan@Kimui-Macmini.local>
2022-03-25 22:20:59 +0900

    템플릿 예제

--- www/example/js/template.js
+++ www/example/js/template.js
@@ -1,18 +1,16 @@
 import Template from '../../flexvue/core/template.class.js';
-import {_template} from '../tpl/template.js';
+import {_template, _testtpl} from '../tpl/template.js';
 
 
 const onReady = () => 
 {
-    // Log.d(_template);
+    Log.d(_testtpl);
     // const htmlUrl = new URL('../tpl/test.html', import.meta.url).href;
     // fetch(htmlUrl).then(response => alert(response.text()));
+    
 
-    // 템플릿 파일명과 템플릿 id
+    // 1. 템플릿 파일 가지고 오기
     const template_filename = new Template().chroot(`../tpl/test.html`,import.meta.url);
-    const template_id = '#tpl_test';
-
-    // 템플릿 파일 가지고 오기
     new Template().readFile( template_filename)
     .then( tpl =>{
         // render 
@@ -25,40 +23,31 @@ const onReady = () =>
         Log.e(e);
     });
 
-    new Template().readFile( template_filename, template_id).then( tpl =>{
-        // render 
-        const outhtml_el2 = document.querySelector('#echo_contents2');
-        outhtml_el2.innerHTML = new Template().render( tpl, {
-            name : "홍길동",
-            age : 27
-        } );
-    });
+    
+    // 2. import js 스트림 템플릿 사용
+    const outhtml_el2 = document.querySelector('#echo_contents2');
+    outhtml_el2.innerHTML = new Template().render( _testtpl, {
+        name : "홍길동",
+        age : 27, 
+        msg : [
+            {name : '홍길동'},
+            {name : '유관순'}
+        ]
+    } );
+    
 
     
-    // include template
-    // new Template().include('#tpl_include_test').
-    // then(tpl => {
-        const outhtml_el = document.querySelector('#echo_include_template');
-        outhtml_el.innerHTML = _template({
-            name : "인클루드 템플릿",
-            age : 27,
-            msg : [
-                {name : '홍길동'},
-                {name : '유관순'}
-            ]
-        });
-        // outhtml_el.innerHTML = new Template().render( _template, {
-        //     name : "인클루드 템플릿",
-        //     age : 27,
-        //     msg : [
-        //         {name : '홍길동'},
-        //         {name : '유관순'}
-        //     ]
-        // } );
-    // })
-    // .catch((e)=>{
-    //     Log.e(e);
-    // });
+    // 3. import js 함수 템플릿 사용
+    const outhtml_el = document.querySelector('#echo_include_template');
+    outhtml_el.innerHTML = _template({
+        name : "인클루드 템플릿",
+        age : 27,
+        msg : [
+            {name : '홍길동'},
+            {name : '유관순'}
+        ]
+    });
+
     
 }
 
--- www/example/tpl/template.js
+++ www/example/tpl/template.js
@@ -1,5 +1,6 @@
+// 함수를 사용
 export const _template  = (resp) => `
-<h3>test</h3>
+<h3><함수형 템플릿></h3>
 <p>${resp.name}</p>
 <p>${resp.age}</p>
     ${
@@ -7,3 +8,17 @@ export const _template  = (resp) => `
     <div>${item.name}</div>
     `).join('')}
 `;
+
+// 스트링을 사용
+export const _testtpl = new String(`
+<br /><br />
+<h3> < String 스트링 템플릿 > </h3>
+name : $\{name}
+age : $\{age}
+
+$\{
+    msg.map(item =>\`
+<div>$\{item.name}</div>
+\`).join('')}
+<br /><br />
+`).toString();
