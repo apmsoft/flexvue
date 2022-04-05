a3bb58941d8884f7164a73eb6c8d71b6592b0afe
JongKwan <apmsoft@gmail.com>
2022-04-05 13:12:26 +0900

    update

--- www/example/js/observable.js
+++ www/example/js/observable.js
@@ -8,6 +8,7 @@ const onReady = () =>
     // notify update
     update(message){
       document.querySelector( this.$target).innerHTML = `CLASS A : ${message}`;
+
     }
   }
 
@@ -22,14 +23,36 @@ const onReady = () =>
     }
   }
 
-  window.observable = new Observable(['public','sub']);
+  class C {
+    constructor(){
+      this.history = [];
+    }
+
+    // notify update
+    update(message){
+      this.history.push(message);
+      Log.d(this.history);
+    }
+  }
+
+  window.observable = new Observable(['public','sub','dds']);
   observable.subscribe('public', new A('#public_a'));
   observable.subscribe('public', new B('#public_b'));
+  observable.subscribe('public', new C());
+
   observable.subscribe('sub', new B('#sub_a'));
   observable.subscribe('sub', new B('#sub_b')); // 같은 채널의 중복 클래스 제거
 
-  observable.notify('public', 'public 채널 message 테스트');
+  observable.subscribe('dds', new A('#public_a'));
+
+  // observable.notify('public', {chart:-1, monitor:0});
   observable.notify('sub', 'sub 채널 message 테스트');
+
+  document.getElementById('ddd').addEventListener('change', function(e) {
+    Log.d(this.value);
+    // observable.notify('public', this.value);
+    observable.notify('public', this.value);
+  });
 }
 
 // document ready
