12857a7edc81d2ccefbc8403a50aa1d6f3eacf5d
fancyupsoft <apmsoft@gmail.com>
2024-03-10 13:11:08 +0900

    Froms 클래스 2.0 으로 업데이트

--- www/flexvue/core/forms.class.js
+++ www/flexvue/core/forms.class.js
@@ -1,35 +1,40 @@
-"use strict";
 export default class Forms {
-    constructor (targetId){
+    constructor(targetId) {
+        this.__version = '2.0';
         this.targetId = targetId;
+        this.formElem = document.querySelector(this.targetId); // Store the form element
     }
+
     // submit
     /**
      * 
      * @param {Form id="theForm"} targetId 
      * @param {function()} callback 
      */
-    doSubmit(callback) 
-    {
+    doSubmit(callback) {
         let send_params = null;
-        const formElem = document.querySelector(this.targetId);
-        formElem.addEventListener('submit', function(e) 
-        {
+        this.formElem.addEventListener('submit', function(e) {
             e.preventDefault();
             send_params = {};
 
             // 데이터 필터링
-            const formData = new FormData(formElem);
-            formData.forEach( (val, name) => {
-                let nodeName = (formElem.elements[name].length > 1) ? formElem.elements[name][0].nodeName : formElem.elements[name].nodeName;
+            const formData = new FormData(this.formElem);
+            formData.forEach((val, name) => {
+                let nodeName = (this.formElem.elements[name].length > 1) ? this.formElem.elements[name][0].nodeName : this.formElem.elements[name].nodeName;
                 let _value = (nodeName == 'TEXTAREA') ? encodeURIComponent(val) : val;
-                send_params[name] = (typeof send_params[name] !== 'undefined' && send_params[name]!='') ? `${send_params[name]},${_value}` : _value;
+                send_params[name] = (typeof send_params[name] !== 'undefined' && send_params[name] != '') ? `${send_params[name]},${_value}` : _value;
             });
-            Log.d (send_params);
 
-            if(typeof callback === 'function'){
+            if (typeof callback === 'function') {
                 callback(send_params);
             }
         });
+
+        return this;
+    }
+
+    // 전송
+    submit() {
+        this.formElem.submit();
     }
 }
